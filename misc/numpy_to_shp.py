e7e4bb2486feaa8c9cd7e4f2b6218c39b1dc0c94
Brent Pedersen - Bioinformatics <bpederse@gmail.com>
2007-10-22 05:20:03 +0000

    add mask so values outside of a watershed are not rendered

--- misc/numpy_to_shp.py
+++ misc/numpy_to_shp.py
@@ -4,12 +4,12 @@ import os
 os.environ['HOME'] = '/tmp/'
 import matplotlib
 matplotlib.use('Agg')
-from pylab import contour, show
+from pylab import contour
 import cPickle
 import ogr
 import tempfile
 
-def numpy_to_shape(x, y, z, shpname=None, epsg=54004, group='', levels=None):
+def numpy_to_shape(x, y, z, shpname=None, epsg=None, group='', levels=None, mask_wkt=''):
     """turn a numpy grid array into a shapefile. 
     x, y, z are grids (likely from meshgrid (x, y) and interpolation (z)
     of the original data. 
@@ -18,16 +18,21 @@ def numpy_to_shape(x, y, z, shpname=None, epsg=54004, group='', levels=None):
     epsg: the projection.
     group: to add a new group (likely a new shpname merits a group indication
     levels: same as for pylab.contour 
+    mask_wkt: e.g. the watershed to clip on. stops contours from appearing outside the watershed.
+
     """
     FIELD_NAME = 'contour' # the field name for the z contour level.
     GROUP_NAME = 'group'   # the name for different groups of contours.
     
+    if mask_wkt:
+        mask_wkt = ogr.CreateGeometryFromWkt(mask_wkt)
+
     if not shpname:
         # could have problems because not using mkstemp...
         shpname = tempfile.mktemp(dir='/var/www/ms_tmp', suffix='.shp')
 
     # pylab.contour does the work of figuring out the contours.
-    cont = contour(x, y, z, levels=levels, colors='k')
+    cont = contour(x, y, z, levels=levels, colors='k', origin='lower')
     shp = ogr.GetDriverByName('ESRI Shapefile')
 
 
@@ -40,6 +45,7 @@ def numpy_to_shape(x, y, z, shpname=None, epsg=54004, group='', levels=None):
         layer = data_source.CreateLayer('', geom_type=ogr.wkbLineString)
 
         field = ogr.FieldDefn(FIELD_NAME, ogr.OFTReal)
+        field.SetWidth(4)
         field.SetPrecision(2)
         layer.CreateField(field)
         group_field = ogr.FieldDefn(GROUP_NAME, ogr.OFTString)
@@ -54,6 +60,8 @@ def numpy_to_shape(x, y, z, shpname=None, epsg=54004, group='', levels=None):
 
         pts = ",".join(["%f %f" % (x, y) for x,y in c.get_verts()])
         geom = ogr.CreateGeometryFromWkt(line_str % pts)
+        if mask_wkt:
+            geom = geom.Intersection(mask_wkt)
 
         f.SetField(FIELD_NAME, cont.levels[i])
         f.SetField(GROUP_NAME, group)
@@ -64,11 +72,12 @@ def numpy_to_shape(x, y, z, shpname=None, epsg=54004, group='', levels=None):
     data_source.Destroy()
 
     # write the projection file. as ogr wont do it for us.
-    proj = ogr.osr.SpatialReference()
-    proj.ImportFromEPSG(epsg)
-    proj_file = open(shpname[:-4] + ".prj", 'w')
-    proj_file.write(proj.ExportToWkt())
-    proj_file.close()
+    if epsg:
+        proj = ogr.osr.SpatialReference()
+        proj.ImportFromEPSG(epsg)
+        proj_file = open(shpname[:-4] + ".prj", 'w')
+        proj_file.write(proj.ExportToWkt())
+        proj_file.close()
 
     return shpname
 
