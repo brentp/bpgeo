e1ceb4381702e7b3b69a8feaee4d2bfd4f082339
Brent Pedersen - Bioinformatics <bpederse@gmail.com>
2008-01-24 04:43:01 +0000

    change regexp &LAYERS to match ?LAYERS, by only using 'LAYERS' in the re

--- google_wms/google-wms.js
+++ google_wms/google-wms.js
@@ -162,7 +162,7 @@ GMap2.prototype.getFeatureInfo = function(pt,wmslayer){
              + '&BBOX=' + bds
              + '&INFO_FORMAT=text/html';
 
-    var ma = url.match(/(LAYERS=[^&?]+)/)[0];
+    var ma = url.match(/(LAYERS=[^&]+)/)[0];
     url += ma.replace('LAYERS','QUERY_LAYERS');
 
     var self = this;
