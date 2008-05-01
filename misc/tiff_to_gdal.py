1bd04f444de1d27d0c6f492419e70652f4144776
Brent Pedersen - Bioinformatics <bpederse@gmail.com>
2008-05-01 01:21:03 +0000

    fix arr to tiff

--- misc/tiff_to_gdal.py
+++ misc/tiff_to_gdal.py
@@ -1,8 +1,8 @@
 
 
 from tempfile import NamedTemporaryFile
-import gdal
-import gdal_array
+from osgeo import gdal, gdal_array
+from osgeo.gdalconst import GDT_Float64
 
 def array_to_tiff(arr, bbox, fn=None):
     d = gdal.GetDriverByName("GTiff")
@@ -22,14 +22,32 @@ def array_to_tiff(arr, bbox, fn=None):
     gdal_array.BandWriteArray(dst.GetRasterBand(1),z)
     return fn
 
+
+def arr_to_tiff(a, fn, xmin, ymin, xmax, ymax):
+    """ a: numpy array
+        fn: a file name to write the tiff
+        xmin, ymin, xmax, ymax: the geographical extents of the array
+    """
+    driver = gdal.GetDriverByName('GTiff')
+    out = driver.Create(fn, a.shape[0], a.shape[1], 1, GDT_Float64)
+
+    out.SetGeoTransform([xmin
+                    , (xmax - xmin)/a.shape[0]
+                    , 0
+                    , ymin
+                    , 0
+                    , (ymax - ymin)/a.shape[1]])
+
+    gdal_array.BandWriteArray(out.GetRasterBand(1), a)
+
+
 if __name__ == "__main__":
 
-    import cPickle
+    import numpy
+    xsize, ysize = 10, 10
+    a = numpy.random.random((xsize, ysize)).astype(numpy.float64)
 
-    p = cPickle.load(open('z.pickle'))
-    bbox = map(float, p['bbox'])
-    z = p['z']
-
-    print array_to_tiff(z, bbox)
-
-
+    xmin, xmax = -121., -119.
+    ymin, ymax = 41., 43.
+      
+    arr_to_tiff(a, 'a.tiff', xmin, ymin, xmax, ymax)
